701bb41f5a88a951f095391e1f2d32d0838d3497
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-07-09 14:53:12 +0900

    一般ユーザの予約取得機能

--- src/api/handler/reservation/get_reservations.go
+++ src/api/handler/reservation/get_reservations.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"reservation-manager/db/generated"
+	"reservation-manager/middleware"
 )
 
 
@@ -22,5 +23,28 @@ return func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "JSONエンコードに失敗しました", http.StatusInternalServerError)
 		return
 	}
+	}
+}
+
+//一般ユーザが予約情報を取得する関数
+func GenGetReservationHandler(db *generated.Queries) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
+		//headerからユーザid取得
+		userID,ok := middleware.UserIDFromContext(r.Context())
+		if !ok {
+			http.Error(w,"UserIDが取得できません",http.StatusUnauthorized)
+			return
+		}
+
+		reservations,err := db.GetReservationByID(r.Context(),int32(userID))
+		if err != nil {
+			http.Error(w,"ユーザidから予約情報の取得に失敗しました",http.StatusForbidden)
+			return
+		}
+		w.Header().Set("Content-Type","application/json")
+		if err := json.NewEncoder(w).Encode(reservations); err != nil {
+			http.Error(w, "JSONエンコードに失敗しました", http.StatusInternalServerError)
+			return
+		}
 	}
 }
