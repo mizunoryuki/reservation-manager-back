c89e0b4fd7c1d76b0b182fe804799a2c3c10ca7b
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-07-09 11:27:48 +0900

    予約情報を削除機能

--- src/api/handler/reservation/delete_reservation.go
+++ src/api/handler/reservation/delete_reservation.go
@@ -1,1 +1,49 @@
 package reservation
+
+import (
+	"net/http"
+	"reservation-manager/db/generated"
+	"reservation-manager/middleware"
+	"strconv"
+	"strings"
+)
+
+func DeleteReservationHandler(db *generated.Queries) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
+		//ユーザid取得
+		userID,ok := middleware.UserIDFromContext(r.Context())
+		if !ok {
+			http.Error(w,"UserIDが取得できません",http.StatusUnauthorized)
+			return
+		}
+
+		//パスパラメータから予約id取得
+		path := r.URL.Path
+		parts := strings.Split(path,"/")// `/admin/reservations/:id`
+		res_id_str := parts[len(parts)-1]
+		res_id_int, err := strconv.Atoi(res_id_str)
+		if err != nil {
+			http.Error(w, "予約idが不正です", http.StatusBadRequest)
+			return
+		}
+		//予約idが正しいか
+		_,err = db.GetReservationByID(r.Context(),int32(res_id_int))
+		if err != nil {
+			http.Error(w,"指定された店舗idが存在しません",http.StatusBadRequest)
+			return
+		}
+
+
+		// 予約作成
+		//sqlcで生成したデータ構造に変換する
+		err  = db.DeleteReservationAsAdmin(r.Context(),int32(userID))
+		if err != nil {
+			http.Error(w,"予約に失敗しました",http.StatusInternalServerError)
+			return
+		}
+
+
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(`{"message":"正常に予約登録ができました"}`))
+	}
+}
