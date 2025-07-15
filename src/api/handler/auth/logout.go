20b30808f4dd92ab9db6dd23e822dc6bbe0136fd
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-07-15 11:23:26 +0900

    ログアウト機能

--- src/api/handler/auth/logout.go
+++ src/api/handler/auth/logout.go
@@ -2,14 +2,26 @@ package auth
 
 import (
 	"net/http"
+	"time"
 
 	"reservation-manager/db/generated"
-
 )
 
+//トークンの無効化
 func LogOutHandler(db *generated.Queries) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(`{"message":"ログアウト"}`))
+		http.SetCookie(w, &http.Cookie{
+			Name:     "access_token",
+			Value:    "",
+			Path:     "/",
+			HttpOnly: true,
+			Secure:   true, 
+			SameSite: http.SameSiteLaxMode,
+			Expires:  time.Unix(0, 0),
+			MaxAge:   -1,
+		})
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message":"logged out"}`))
 	}
 }
